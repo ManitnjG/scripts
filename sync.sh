5da1ded323f59eef78391ebf7ee6f743703d5f77
xc112lg <dtiven13@gmail.com>
2024-01-25 05:31:49 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,15 +4,12 @@
 rm -rf frameworks/base/
 rm -rf device/lge/
 rm -rf hardware/lge
-rm -rf kernel/lge/msm8996/
-rm -rf vendor/lge/
-rm -rf vendor/lineage/
 rm out/target/product/*/*.zip
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
 source scripts/fixes.sh
-lunch lineage_h870-userdebug
+lunch lineage_h872-userdebug
 m -j$(nproc --all) bacon
 #lunch lineage_us997-userdebug
 #m bacon
