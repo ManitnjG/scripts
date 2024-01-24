7808b5549f19b05000b465e0ffa468b67d28737a
xc112lg <dtiven13@gmail.com>
2024-01-25 05:35:36 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -5,6 +5,10 @@ rm -rf frameworks/base/
 rm -rf device/lge/
 rm -rf hardware/lge
 rm out/target/product/*/*.zip
+rm -rf .repo/local_manifests
+repo init -u https://github.com/crdroidandroid/android.git -b 13.0 --git-lfs
+mkdir .repo/local_manifests
+cp scripts/roomservice.xml .repo/local_manifests
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 source build/envsetup.sh
 
