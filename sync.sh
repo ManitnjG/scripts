d5072f8ee1bafdd7b40a94b690933c741eb6c2d1
xc112lg <dtiven13@gmail.com>
2024-01-25 08:54:19 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -15,9 +15,9 @@ source build/envsetup.sh
 source scripts/fixes.sh
 lunch lineage_h872-userdebug
 m -j$(nproc --all) bacon
-#lunch lineage_us997-userdebug
-#m bacon
-#lunch lineage_h872-userdebug
-#m bacon
+lunch lineage_us997-userdebug
+m -j$(nproc --all) bacon
+lunch lineage_h872-userdebug
+m -j$(nproc --all) bacon
 
 
