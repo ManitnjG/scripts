1d77ad63c99a6cbdf6aaf1b844c2c4f8599da47f
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-03-15 05:25:26 +0800

    test

--- sync.sh
+++ sync.sh
@@ -68,9 +68,9 @@ if [ "$DEVICE" == "all" ]; then
 elif [ "$DEVICE" == "h872" ]; then
     echo "Building for h872..."
 
-    lunch lineage_h872-userdebug
-    m installclean
-    m -j$(nproc --all) bacon
+ #   lunch lineage_h872-userdebug
+ #   m installclean
+ #   m -j$(nproc --all) bacon
 else
     echo "Building for the specified device: $DEVICE..."
     # Build for the specified device
