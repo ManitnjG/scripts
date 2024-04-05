2d8a930f26defbd8f0d3844d8a3e7cc5ffd40ecc
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-06 06:23:34 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -10,7 +10,7 @@
 mkdir -p workspace && cd workspace
   git clone --depth=1 https://github.com/Jiovanni-dump/xiaomi_ishtar_dump -b missi_phone_global-user-13-TKQ1.221114.001-V14.0.1.0.TMAMIXM-release-keys ./firmware-dump
 
-  git clone --depth=1 https://github.com/jacksproject/device_xiaomi_ishtar -b 13.0 ./android/device/ishtar/xiaomi
+  git clone --depth=1 https://github.com/HurtCopain/device_xiaomi_sm8550-common -b lineage-20 ./android/device/xiaomi/sm8550-common
 
 
   git clone --depth=1 https://github.com/LineageOS/android_tools_extract-utils -b lineage-20.0 ./android/tools/extract-utils
@@ -19,8 +19,8 @@ mkdir -p workspace && cd workspace
   echo "Done cloning extract-tools."
 
 
-  chmod a+x android/device/ishtar/xiaomi/setup-makefiles.sh
-  cd android/device/ishtar/xiaomi/
+  chmod a+x android/device/xiaomi/sm8550-common/setup-makefiles.sh
+  cd android/device/xiaomi/sm8550-common
   bash extract-files.sh ../../../../firmware-dump/
   echo "Done extracting and making files."
   echo "Pushing as repository now."
