2dc931a101cc4d75008278e182042a4a777f2c6a
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-06 15:38:31 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git clone rm -rf dumpyara;crave ssh -- git clone https://github.com/romdumpit/dumpyara
+rm -rf dumpyara;git clone https://github.com/romdumpit/dumpyara
 bash dumpyara/setup.sh 
 bash dumpyara/dumpyara.sh "https://bn.d.miui.com/V14.0.6.0.TMATWXM/miui_ISHTARTWGlobal_V14.0.6.0.TMATWXM_41d3a5acf9_13.0.zip" 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
 
