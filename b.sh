9ee0664e592e2fb9a47862800f3a06205ee2ca87
xc112lg <152837944+xc112lg@users.noreply.github.com>
2024-04-06 16:26:44 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -2,7 +2,7 @@
 
 rm -rf dumpyara;git clone https://github.com/romdumpit/dumpyara
 bash dumpyara/setup.sh 
-bash dumpyara/dumpyara.sh "https://bn.d.miui.com/V14.0.20.0.TMACNXM/ishtar_images_V14.0.20.0.TMACNXM_20231011.0000.00_13.0_cn_chinatelecom_b08074c273.tgz" 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
+bash dumpyara/dumpyara.sh "https://bn.d.miui.com/V14.0.20.0.TMACNXM/ishtar_images_V14.0.20.0.TMACNXM_20231011.0000.00_13.0_cn_chinatelecom_b08074c273.tgz" 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
 
 
 
